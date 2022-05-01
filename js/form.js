8386bf9915846130b349f8df0b898059080aebda
YzanBaezz <yz.baez@duocuc.cl>
2022-04-30 21:04:01 -0400

    cambios-msg-error

--- js/form.js
+++ js/form.js
@@ -96,5 +96,8 @@ formulario.addEventListener('submit' , (e) => {
         });
     } else {
         document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        setTimeout(() => {
+        document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+        }, 3000);
     }
 });
